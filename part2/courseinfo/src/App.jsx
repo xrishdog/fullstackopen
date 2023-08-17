45dacbac011831c5966cfc817be3348b12872dba
xrishdog <108433356+xrishdog@users.noreply.github.com>
2023-08-17 14:29:17 -0400

    Rendered components using map function, ex2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,54 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+
+const Part = ({ part }) => 
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = ({ parts }) => {
+  console.log(parts.map(part => part.name))
+  return(
+    <>
+      {parts.map(part => <Part key = {part.id} part = {part}/>)}
+    </>
+  )
+}
+
+const Course = ({course}) => {
+  return(
+    <>
+      <Header course = {course.name} />
+      <Content parts = {course.parts} />
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
