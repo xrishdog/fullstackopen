4f755209f846e25247738ef9488ede0a5befa677
xrishdog <108433356+xrishdog@users.noreply.github.com>
2023-08-21 16:17:00 -0400

    Added phone number entry feature

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,16 +5,18 @@ const App = () => {
     { name: 'Arto Hellas', id: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState ('')
 
   const submitInfo = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
+      phone: newPhone,
       id: newName
     }
 
     const alreadyExists = 
-      persons.some(person => JSON.stringify(person) === JSON.stringify(personObject)) 
+      persons.some(person => JSON.stringify(person.name) === JSON.stringify(newName)) 
     if (alreadyExists) {
       alert(`${newName} is already added to the phonebook`)
     }
@@ -22,12 +24,17 @@ const App = () => {
       setPersons(persons.concat(personObject))
     }
     setNewName('')
+    setNewPhone('')
     console.log('button clicked', event.target)
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handlePhoneChange = (event) =>{
+    console.log(event.target.value)
+    setNewPhone(event.target.value)
   }
 
   return (
@@ -38,11 +45,14 @@ const App = () => {
           name: <input value = {newName} onChange = {handleNameChange}/>
         </div>
         <div>
+          number: <input value = {newPhone} onChange = {handlePhoneChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key = {person.id}>{person.name}</div>)}
+      {persons.map(person => <div key = {person.id}>{person.name} {person.phone}</div>)}
     </div>
   )
 }
